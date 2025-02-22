1384004f25a94f25055a0621805782d94428ef2c
sailorseahorse <jquiruz1@gmail.com>
2025-02-21 17:46:46 -0800

    Completed up to problem 10 in Problem Set Version 2

--- unit1/unit1_session2_psv2.py
+++ unit1/unit1_session2_psv2.py
@@ -27,10 +27,22 @@ def main():
     # print(missing_num)
 
 
-    list = [1, 2, 3, 4, 5]
-    # print(list)
-    reversed_list = reverse_list(list)
-    print(reversed_list)
+    # list = [1, 2, 3, 4, 5]
+    # # print(list)
+    # reversed_list = reverse_list(list)
+    # print(reversed_list)
+
+
+    # nums = [2, 5, 1, 8, 6, 5, 3, 3, 3]
+    # odd_nums = get_odds(nums)
+    # print(odd_nums)
+
+
+    # multiplication_table(7)
+
+    list_to_number([1, 0, 3])
+
+
 
 
 
@@ -105,7 +117,7 @@ def find_missing(list):
 
 # Problem 6: Reverse List
 
-# *** Understand ***
+# *** Understand *************************
 
 # This is a function in which a client inputs a list of integers
 # and returns a new list containing the integers of the parameter
@@ -140,6 +152,8 @@ def find_missing(list):
 # for loop, iterate in reverse order
 # append method
 
+# ************ PLAN ***************************
+
 # --------------------
 # Function Steps
 # --------------------
@@ -150,12 +164,121 @@ def find_missing(list):
 #   # Append the element to the new list
 # Return the new list
 
+# ************ IMPLEMENT ************************
+
 def reverse_list(list):
     result = []
     for i in range(len(list) - 1, -1, -1):
         result.append(list[i])
     
     return result
+
+
+
+# Problem 7: Get Odd Numbers
+
+# UNDERSTAND
+
+# This function takes an input LIST of INTEGERS as a parameter
+# and RETURNS a NEW LIST of ODD elements from the input.
+
+# Input, Datatypes: a list of integers (sign unspecified)
+# Output, Datatypes: a list of odd integers (sign unspecified)
+# Calculcations, Control Flow: for-loop, no methods
+
+# PLAN
+
+# Define a function called "Get odds"
+# Define a list of integers called "numbers" as a parameter
+# Inside function, reserve an empty new list called "result" for odd integers
+# Check each element in the list parameter, "numbers"
+#   # If the current element is not divisible by 2
+#   #   # Add the element to the new list, "result"
+# Return the new list "result" to the client
+
+# IMPLEMENT
+
+def get_odds(numbers):
+    result = []
+    for i in numbers:
+        if i % 2 != 0:
+            result.append(i)
+
+    return result
+
+
+
+# Problem 8: Multiplication Table
+
+# UNDERSTAND
+
+# This function takes a SINGLE INTEGER parameter from the client
+# and PRINTS the MULTIPLES of that integer 10 times
+
+# Input, Datatypes:
+#   # "number", integer
+# Output, Datatypes: 
+#   # 10 numbers that are multiples of the input, integers
+# Calculation, Control Flow: 
+#   # multiple of "number" = "number" + "number"
+#   # for-loop using range method
+
+# PLAN
+
+# Define a function called "multiplication table"
+# Declare a parameter called "number"
+# Declare a variable called "sum" and set it to 0
+# Declare a for loop that will repeat 10 times
+#   # Add the parameter "number" to the "sum" and assign the value to "sum"
+#   # Print the "sum" to the console
+
+# IMPLEMENT
+
+def multiplication_table(number):
+    sum = 0
+    for i in range(1, 11):
+        sum = number + sum
+        print(sum)
+
+
+
+# Problem 9: Create Number
+
+# UNDERSTAND
+
+# This function takes a LIST of INTEGERS 
+# BETWEEN 0 AND 9 (inclusive/exclusive unspecified) and 
+# combines those integers into a SINGLE INTEGER (NOT a string); 
+# this single integer is then RETURNED to the client.
+
+# Input, Datatypes:
+#   # a list of digits called "digits", list
+#
+# Output, Datatypes:
+#   # a single integer called "result", integer
+#
+# Calculation:
+#   # N/A
+
+# PLAN
+
+# Define a function called "list to a number"
+# Declare a list of integers called "digits" as the parameter
+# Declare a string variable called "result" (to be later converted to an integer)
+# Check each element in the input parameter "digits"
+#   # Add current element to the string "result" by concatenation
+# Convert the datatype of "result" from a string to an integer
+# Return the integer "result" to the client
+
+# IMPLEMENT
+
+def list_to_number(digits):
+    result = ""
+    for i in digits:
+        result = result + str(i)
+
+    result = int(result)
+    print(result)
 
 
 
